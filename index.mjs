d3dc8da96e927f9b09db4f00a524d002826b58a4
Enyerber Franco <enyerverfranco@gmail.com>
2026-05-05 22:40:22 -0400

    feat: universal compatibility, break circularity, and ESM support

--- index.mjs
+++ index.mjs
@@ -1,4 +1,10 @@
 import dbTabla from './index.js';
 
 export default dbTabla;
-export const { Connect, dbResult, dbRow, dbTablaError, procesingSql } = dbTabla;
+export const { 
+    Connect, 
+    dbResult, 
+    dbRow, 
+    dbTablaError, 
+    procesingSql 
+} = dbTabla;
